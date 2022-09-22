64f583dc532d66a9d0bedf3acb4d809e6b0664a2
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-09-22 22:27:03 +0330

    bottom_nav_bar

---
+++ android/app/src/main/kotlin/com/example/flutter_button_nav_bar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_button_nav_bar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,211 @@
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BottomNavBar(),
+    );
+  }
+}
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> _buildScreens() {
+      return [
+        const Screen1(),
+        const Screen2(),
+        const Screen3(),
+        const Screen4(),
+        const Screen5(),
+      ];
+    }
+
+    List<PersistentBottomNavBarItem> _navBarsItems(){
+      return [
+
+        PersistentBottomNavBarItem(
+          icon: const Icon(Icons.home),
+          title: ("Home"),
+          activeColorPrimary: Colors.blue,
+          inactiveColorPrimary: Colors.grey,
+        ), PersistentBottomNavBarItem(
+          icon: const Icon(Icons.favorite_border_rounded),
+          title: ("Favorite"),
+          activeColorPrimary: Colors.blue,
+          inactiveColorPrimary: Colors.grey,
+        ),
+
+        PersistentBottomNavBarItem(
+          icon: const Icon(
+            Icons.shopping_cart,
+            color: Colors.white,
+          ),
+          inactiveIcon: const Icon(
+            Icons.shopping_cart_outlined,
+            color: Colors.white,
+          ),
+          activeColorPrimary: Colors.blue,
+          inactiveColorPrimary: Colors.grey,
+        ),
+
+        PersistentBottomNavBarItem(
+          icon: const Icon(Icons.send),
+          title: ("Send"),
+          activeColorPrimary: Colors.blue,
+          inactiveColorPrimary: Colors.grey,
+        ),PersistentBottomNavBarItem(
+          icon: const Icon(Icons.settings_outlined),
+          title: ("Setting"),
+          activeColorPrimary: Colors.blue,
+          inactiveColorPrimary: Colors.grey,
+        ),
+
+      ];
+
+
+    }
+
+    PersistentTabController controller;
+
+    controller = PersistentTabController(initialIndex: 0);
+    return PersistentTabView(
+      context,
+      screens:_buildScreens(),
+      items: _navBarsItems(),
+      controller: controller,
+      confineInSafeArea: true,
+      backgroundColor: Colors.white,
+      handleAndroidBackButtonPress: true,
+      resizeToAvoidBottomInset: true,
+      stateManagement: true,
+      hideNavigationBarWhenKeyboardShows: true,
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        colorBehindNavBar: Colors.white,
+      ),
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      itemAnimationProperties: const ItemAnimationProperties(
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
+      ),
+      screenTransitionAnimation: const ScreenTransitionAnimation(
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 200),
+      ),
+      navBarStyle:
+      NavBarStyle.style16,
+
+
+    );
+  }
+}
+class Screen1 extends StatelessWidget {
+  const Screen1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Screen1')),
+      body: const Center(
+        child: Text(
+          'Screen1',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}class Screen2 extends StatelessWidget {
+  const Screen2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Screen2')),
+      body: const Center(
+        child: Text(
+          'Screen2',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}class Screen3 extends StatelessWidget {
+  const Screen3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Screen3')),
+      body: const Center(
+        child: Text(
+          'Screen3',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}class Screen4 extends StatelessWidget {
+  const Screen4({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Screen4')),
+      body: const Center(
+        child: Text(
+          'Screen4',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}class Screen5 extends StatelessWidget {
+  const Screen5({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Screen5')),
+      body: const Center(
+        child: Text(
+          'Screen5',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
